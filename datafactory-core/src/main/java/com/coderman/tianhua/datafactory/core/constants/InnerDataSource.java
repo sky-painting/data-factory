bbdbf0f83225b8c61e4fd72afe9b0807ceb207cf
fanchunshuai <2217011466@qq.com>
2020-12-10 23:56:19 +0800

    构建默认数据源

---
+++ datafactory-core/src/main/java/com/coderman/tianhua/datafactory/core/constants/InnerDataSource.java
@@ -0,0 +1,44 @@
+package com.coderman.tianhua.datafactory.core.constants;
+
+/**
+ * 内建公共数据源-code
+ *
+ * 数据字典
+ */
+public interface InnerDataSource {
+    /**
+     * 身份证--
+     * http://www.mamicode.com/info-detail-1832018.html
+     */
+    String CARD_NUMBER = "com.common.user.cardnumber";
+
+    /**
+     * 出生年月日--自建实现
+     */
+    String BIRTHDAY = "com.common.user.birthday";
+
+    /**
+     * 省份名称-code
+     */
+    String PROVINCE = "com.common.area.province";
+
+    /**
+     * 城市名称-code
+     */
+    String CITY = "com.common.area.city";
+
+    /**
+     * 民族
+     */
+    String NATION = "com.common.user.nation";
+
+    /**
+     * 姓氏--自建实现
+     */
+    String LAST_NAME = "com.common.user.lastname";
+
+    /**
+     * 名--自建实现
+     */
+    String FIRST_NAME = "com.common.user.firstname";
+}
